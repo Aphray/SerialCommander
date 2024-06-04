6a7b7677bd48d33e1737a70fd419c9478e29f248
Bryce Kingsley <brycejkingsley@gmail.com>
2024-06-04 12:57:03 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <assert.h>
 
 #define RX_BUFFER_SIZE 128
 #define TX_BUFFER_SIZE 256
@@ -6,13 +7,32 @@
 #include "CommandHandler.h"
 
 
-auto a = TX_BUFFER_SIZE;
+template <typename T> struct TypeInfo { static constexpr char* name = "n/a"; };
+
+#define MAKE_TYPE_INFO(type) template<> struct TypeInfo<type> { static constexpr char* name = #type; }
+
+MAKE_TYPE_INFO(int);
+MAKE_TYPE_INFO(bool);
+MAKE_TYPE_INFO(char*);
+MAKE_TYPE_INFO(const char*);
+MAKE_TYPE_INFO(const __FlashStringHelper*);
+
+constexpr bool strequal(char const * a, char const * b) {
+    return *a == *b && (*a == '\0' || strequal(a + 1, b + 1));
+}
+
+#define ASSERT_TYPE(var, type) static_assert(strequal(TypeInfo<typeof(var)>::name, #type), "Wrong type!")
+
+#define TEST F("test")
 
 void setup() {
+
+  ASSERT_TYPE(TEST, const __FlashStringHelper*);
   Serial.begin(9600);
+
 }
 
 void loop() {
-  Serial.println(RX_BUFFER_SIZE);
-  Serial.println(TX_BUFFER_SIZE);
+  // Serial.println(RX_BUFFER_SIZE);
+  // Serial.println(TX_BUFFER_SIZE);
 }
